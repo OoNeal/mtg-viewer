7972d25493e45351c1efd633a07f268c971222de
OoNeal <arnaud35u@etu.univ-lorraine.fr>
2024-03-18 17:41:12 +0100

    Add search cards

--- src/Controller/ApiCardController.php
+++ src/Controller/ApiCardController.php
@@ -42,4 +42,19 @@ class ApiCardController extends AbstractController
         }
         return $this->json($card);
     }
+
+    #[Route('/search/{name}', name: 'Search card', methods: ['GET'])]
+    #[OA\Parameter(name: 'name', description: 'Name of the card', in: 'path', required: true, schema: new OA\Schema(type: 'string'))]
+    #[OA\Put(description: 'Search a card by name')]
+    #[OA\Response(response: 200, description: 'Show card')]
+    #[OA\Response(response: 404, description: 'Card not found')]
+    public function cardSearch(string $name): Response
+    {
+        $this->logger->info('Search card ' . $name);
+        $cards = $this->entityManager->getRepository(Card::class)->searchByName($name);
+        if (!$cards) {
+            return $this->json(['error' => 'Card not found'], 404);
+        }
+        return $this->json($cards);
+    }
 }
